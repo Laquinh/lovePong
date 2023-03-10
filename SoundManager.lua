77bd36035ab4db209007ec94121a24d8881fa063
Laquin-Laquih <anderaginaga2@gmail.com>
2023-03-10 19:55:23 +0100

    SoundManager as local

--- SoundManager.lua
+++ SoundManager.lua
@@ -1,11 +1,14 @@
-SoundManager = {}
+local SoundManager = {}
 
 SoundManager.padBlipSound = love.audio.newSource('assets/sounds/blip.wav', 'static')
+
 SoundManager.wallBlipSound = love.audio.newSource('assets/sounds/blip.wav', 'static')
 SoundManager.wallBlipSound:setVolume(0.3)
 SoundManager.wallBlipSound:setPitch(1.5)
+
 SoundManager.hitBlipSound = love.audio.newSource('assets/sounds/blip.wav', 'static')
 SoundManager.hitBlipSound:setPitch(2)
+
 SoundManager.missBlipSound = love.audio.newSource('assets/sounds/blip.wav', 'static')
 SoundManager.missBlipSound:setPitch(0.7)
 
